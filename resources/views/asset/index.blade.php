fed3116b8a24206c2181f1fa05fab210a5ac8b39
AkihisaYamada <2014akihisayamada@gmail.com>
2022-07-15 20:44:45 +0900

    Fix Design

--- resources/views/asset/index.blade.php
+++ resources/views/asset/index.blade.php
@@ -11,51 +11,49 @@
                 </div>
                 <div class="card-body">
                     <!-- Chart -->
-                    <div class="card mb-3">
+                    <div class="card mb-4">
                         <div class="card-body">
                             <canvas id="myChart"></canvas>
                         </div>
                     </div>
                     <!-- Asset List -->
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <p class="h5"><strong>Monthly Asset History</strong></p>
-                            <table class="table table-borderless" style="word-break:break-all;">
-                                <thead>
-                                    <tr>
-                                    <th scope="col">Date</th>
-                                    <th scope="col">Bank JP</th>
-                                    <th scope="col">Bank US</th>
-                                    <th scope="col">Cash JP</th>
-                                    <th scope="col">Cash US</th>
-                                    <th scope="col">Stock</th>
-                                    <th scope="col">Total</th>
-                                    <th scope="col"></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($assets as $key => $asset)
-                                    <tr>
-                                        <td>{{ date("m/Y",strtotime($asset->target_date)) }}</td>
-                                        <td>{{ number_format($asset->bank_jpy) }}</td>
-                                        <td>{{ number_format($asset->bank_usd) }}</td>
-                                        <td>{{ number_format($asset->investigation_jpy) }}</td>
-                                        <td>{{ number_format($asset->investigation_usd) }}</td>
-                                        <td>{{ number_format($asset->stock) }}</td>
-                                        <td>{{ number_format($asset->total) }}</td>
-                                        <td>@if(count($assets) > $key+1)
-                                                @if($assets[$key+1]->total > $asset->total)
-                                                    <scan class="text-danger"><i class="fa-solid fa-arrow-down"></i></scan>
-                                                @else
-                                                    <scan class="text-primary"><i class="fa-solid fa-arrow-up"></i></scan>
-                                                @endif
+                    <div class="mb-1">
+                        <p class="h4"><strong>Monthly Asset History</strong></p>
+                        <table class="table table-borderless" style="word-break:break-all;">
+                            <thead>
+                                <tr>
+                                <th scope="col">Date</th>
+                                <th scope="col">Bank JP</th>
+                                <th scope="col">Bank US</th>
+                                <th scope="col">Cash JP</th>
+                                <th scope="col">Cash US</th>
+                                <th scope="col">Stock</th>
+                                <th scope="col">Total</th>
+                                <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($assets as $key => $asset)
+                                <tr>
+                                    <td>{{ date("m/Y",strtotime($asset->target_date)) }}</td>
+                                    <td>{{ number_format($asset->bank_jpy) }}</td>
+                                    <td>{{ number_format($asset->bank_usd) }}</td>
+                                    <td>{{ number_format($asset->investigation_jpy) }}</td>
+                                    <td>{{ number_format($asset->investigation_usd) }}</td>
+                                    <td>{{ number_format($asset->stock) }}</td>
+                                    <td>{{ number_format($asset->total) }}</td>
+                                    <td>@if(count($assets) > $key+1)
+                                            @if($assets[$key+1]->total > $asset->total)
+                                                <scan class="text-danger"><i class="fa-solid fa-arrow-down"></i></scan>
+                                            @else
+                                                <scan class="text-primary"><i class="fa-solid fa-arrow-up"></i></scan>
                                             @endif
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                        @endif
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
