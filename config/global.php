22a05ed8ba0dcd86264aa13cf802247a9148c024
AkihisaYamada <2014akihisayamada@gmail.com>
2022-08-13 22:01:25 -0700

    Change Setting

--- config/global.php
+++ config/global.php
@@ -1,11 +1,11 @@
 <?php
 return [
 
-    'api_url' => 'http://10.0.0.119/data/get_api?',
-    'api_key' => '6I3D2ZtLAp',
+    'api_url' => 'test',
+    'api_key' => 'test',
     'pagination' => 25,
 
-    'instagram_url' => 'https://graph.facebook.com/v13.0/',
-    'instagram_fields' => 'id,caption,media_url,media_type,user_tags,image_url,like_count,timestamp,permalink,username',
+    'instagram_url' => 'test',
+    'instagram_fields' => 'test',
 
 ];
